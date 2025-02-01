469ab9c5f23e8fedede1d51787a0f36fd1e0f2ba
wada <wadayugo@keio.jp>
2025-02-01 22:22:18 +0900

    fix: fix script

--- fetch-repo-tree.sh
+++ fetch-repo-tree.sh
@@ -1,15 +1,16 @@
-# receive owner and repo name as arguments
+# receive owner, repo and default branch name as arguments
 # validate arguments
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <owner> <repo>"
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <owner> <repo> <branch>"
   exit 1
 fi
 
 # set variables
 owner=$1
 repo=$2
+branch=$3
 
 gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/$owner/$repo/git/trees/master
+  /repos/$owner/$repo/git/trees/$branch?recursive=1
